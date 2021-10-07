dd4a04aa2d15da9dbae53d75106547343ccdc46e
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-10-07 21:54:21 +0530

    initialized and set up pages and did required installations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space} from "antd";
+import {Navbar, Exchanges, HomePage,Cryptocurrencies,CryptoDetails, News} from './components';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route path="/" component={HomePage} exact/>
+              <Route path="/exchanges" component={Exchanges} exact/>
+              <Route path="/cryptocurrencies" component={Cryptocurrencies} exact/>
+              <Route path="/crypto/:coinId" component={CryptoDetails} exact/>
+              <Route path="/news" component={News} exact/>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar} from "./Navbar";
+export {default as HomePage} from "./Pages/Homepage";
+export {default as News } from "./Pages/News";
+export {default as Exchanges} from "./Pages/Exchanges";
+export {default as Cryptocurrencies} from "./Pages/Cryptocurrencies";
+export { default as CryptoDetails} from "./Pages/CryptoDetails";
