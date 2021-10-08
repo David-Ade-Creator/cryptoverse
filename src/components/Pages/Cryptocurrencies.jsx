f40e1ae39a41dd12fcc6e1f279e18540d332e2db
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-10-08 15:47:19 +0530

    homepage completed also implemented redux-toolkit for state management

--- src/components/Pages/Cryptocurrencies.jsx
+++ src/components/Pages/Cryptocurrencies.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import {Link} from 'react-router-dom';
+import {Card, Row, Col, Input} from "antd";
 
-function Cryptocurrencies() {
+import { useGetCryptosQuery} from "../Services/cryptoApi";
+
+function Cryptocurrencies({simplified}) {
+    const count = simplified ? 10 : 100;
+    const {data : cryptosList, isFetching} = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = React.useState(cryptosList?.data?.coins)
+
     return (
-        <div>
-            Cryptocurrencies
-        </div>
+        <>
+        <Row gutter={[32,32]} className="crypto-card-container">
+            {cryptos.map((currency) => (
+                <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                    <Link to={`/crypto/${currency.id}`}>
+                        <Card title={`${currency.rank}. ${currency.name}`} 
+                        extra={<img className="crypto-image" src={currency.iconUrl} alt={currency.name}/>}
+                        hoverable
+                        >
+                            <p>Price : {millify(currency.price)}</p>
+                            <p>Market Cap : {millify(currency.marketCap)}</p>
+                            <p>Daily Change : {millify(currency.change)}%</p>
+                        </Card>
+                    </Link>
+                </Col>
+            ) )}
+        </Row>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter as Router} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "./app/store";
 import "antd/dist/antd.css";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
